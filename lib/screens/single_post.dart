e798c9a17542ac8234968208c7476b8aa4c689c1
Mohamed-Tarek4 <mohamedana17327@gmail.com>
2020-08-29 23:49:24 +0200

    design single_post widget

--- lib/screens/single_post.dart
+++ lib/screens/single_post.dart
@@ -10,11 +10,46 @@ class SinglePost extends StatefulWidget {
 class _SinglePostState extends State<SinglePost> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.teal,
-      ),
+    return CustomScrollView(
+
+          slivers: <Widget>[
+            SliverAppBar(
+              expandedHeight: 250,
+              floating: true,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(widget.article.imageURL),
+                      fit: BoxFit.cover,
+                    )
+                  ),
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+            (context, index){
+              if(index ==0){
+                return Container(
+                  height: 500,
+                  color: Colors.teal,
+                );
+
+              }else{
+              return Container(
+                height: 250,
+                color: Colors.amberAccent,
+              );}
+            },
+    // Builds 1000 ListTiles
+    childCount: 350,
+    ),
+              ),
+
+          ],
+
     );
   }
 }
