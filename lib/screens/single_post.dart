a7300854c054321bab0f5f20d7eff09db4378e21
Mohamed-Tarek4 <mohamedana17327@gmail.com>
2020-08-30 22:40:01 +0200

    design single_post widget

--- lib/screens/single_post.dart
+++ lib/screens/single_post.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:breakingnews/api/all_articles.dart';
+
 class SinglePost extends StatefulWidget {
  Article article;
  SinglePost(this.article);
@@ -10,46 +12,122 @@ class SinglePost extends StatefulWidget {
 class _SinglePostState extends State<SinglePost> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
+    return Scaffold(
+      body: CustomScrollView(
 
-          slivers: <Widget>[
-            SliverAppBar(
-              expandedHeight: 250,
-              floating: true,
-              pinned: true,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(widget.article.imageURL),
-                      fit: BoxFit.cover,
-                    )
+            slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: 250,
+                floating: true,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(widget.article.imageURL),
+                        fit: BoxFit.cover,
+                      )
+                    ),
                   ),
                 ),
               ),
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+              (context, index){
+                if(index ==0){
+                  return _drawPostDetails();
+                }else if(index>0&&index<=25){
+    return _comments();
+                }else{
+                return _addComment();
+                }
+              },
+
+      childCount: 27,
+      ),
+                ),
+
+            ],
+
+      ),
+    );
+  }
+
+  Widget _drawPostDetails() {
+    return Flexible(
+      child: Container(
+        padding: EdgeInsets.all(8.0),
+        child: Text(widget.article.desc,style: TextStyle(
+          fontSize: 18.0,
+          height: 1.1,
+          letterSpacing: 1.2
+        ),),
+      ),
+    );
+  }
+
+  Widget _comments() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+
+            height: 2.0,
+            color: Colors.grey.shade100,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom:8.0,top: 8.0),
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right:8.0),
+                  child: CircleAvatar(
+                    backgroundImage: ExactAssetImage('assets/images/whatnew.jpg'),
+                  ),
+                ),
+                Column(crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text("the publisher"),
+                  Text("time"),
+                ],)
+              ],
             ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-            (context, index){
-              if(index ==0){
-                return Container(
-                  height: 500,
-                  color: Colors.teal,
-                );
+          ),
+            Text("the comment on the article"),
+        ],
+      ),
+    );
+  }
 
-              }else{
-              return Container(
-                height: 250,
-                color: Colors.amberAccent,
-              );}
-            },
-    // Builds 1000 ListTiles
-    childCount: 350,
-    ),
-              ),
+  Widget _addComment() {
+    return Container(
+      color: Color.fromRGBO(241, 245, 247, 1),
+      child: Row(
+      children: [
+        Flexible(
+          child: TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText:'Write your Comment here!',
 
-          ],
+              filled: true,
+              contentPadding: EdgeInsets.only(left: 16,top: 16,bottom: 16)
+            ),
+          ),
+        ),
+        FlatButton(
+          child: Text("SEND",style: TextStyle(
+            color: Colors.blue,
+            fontSize: 18
+          ),),
+          onPressed: (){
 
+          },
+        )
+      ],
+      ),
     );
   }
 }
